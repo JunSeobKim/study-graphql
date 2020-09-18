a6144cdc005846a0b4f17615eeaf3bf866ba81a1
MainDesktop_KimJunSeob <wnstjq616@gmail.com>
2020-09-18 22:11:13 +0900

    Refactoring graphql

---
+++ graphql/resolvers.js
@@ -0,0 +1,8 @@
+// 보낸 요청에 따라 database에서 원하는 데이터를 받아올 수 있도록 핸들링 할 수 있는 역할
+
+const resolvers = {
+  Query: {
+    name: () => "junseobkim",
+  },
+};
+export default resolvers;
--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers";
 require("dotenv").config();
 
 const PORT = process.env.PORT;
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
-`;
 
-const resolvers = {
-  Query: {
-    hello: (_, { name }) => `Hello ${name || "World"}`,
-  },
-};
+const server = new GraphQLServer({
+  typeDefs: "graphql/schema.graphql",
+  resolvers, // resolvers: resolvers 의 es6 문법
+});
 
-const server = new GraphQLServer({ typeDefs, resolvers });
 server.start({ port: PORT }, () =>
   console.log(`Server is running on http://localhost:${PORT}`)
 );
