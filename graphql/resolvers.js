334d426ea49a64a02c94267bc7605b53e8aacd4f
김준섭 <kimjunseob@gimjunseob-ui-MacBookPro.local>
2020-09-19 23:42:14 +0900

    Practice graphql

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,11 @@
 // 보낸 요청에 따라 database에서 원하는 데이터를 받아올 수 있도록 핸들링 할 수 있는 역할
+import { people, getById } from "./db";
 
 const resolvers = {
   Query: {
-    name: () => "junseobkim",
+    person: () => people,
+    people: (_, { id }) => getById(id),
   },
 };
+
 export default resolvers;
